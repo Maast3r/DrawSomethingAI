e0766ff6d3d154672da85812ebe7d3ed474ba43a
Andrew Ma <maastergotamil@gmail.com>
2017-02-08 19:21:00 -0500

    now reads all images and outputs to csv

--- cleanImages.jl
+++ cleanImages.jl
@@ -2,58 +2,54 @@ using Images
 using DataFrames
 using FileIO
 
-println("Loading picture..")
-img = load("/raided/datasets/sketches_png/png/airplane/1.png")
-img2 = load("/raided/datasets/sketches_png/png/airplane/2.png")
-println("Done loading picture. Now extracting pixel values...\n")
+## THERE ARE 250 different sketches, with 80 sketches each
 
-println("Converting image to array of strings...")
-# img = map(x -> x.val, img)
-img = map(x-> x.val, img)
-img2 = map(x-> x.val, img2)
-println("img is now an array of strings\n")
-
-println("Reshape img to 1d array...")
-transpose = reshape(img, (1234321, 1))
-transpose2 = reshape(img2, (1234321, 1))
-println(transpose[1])
-println(ndims(transpose))
-data = ["airplane"]
-data2 = ["airplane"]
-println(data[1])
-println(ndims(data))
-println("Done reshaping\n")
-
-println("Appending tranpose of image to data")
-data = vcat(data, transpose)
-data2 = vcat(data2, transpose2)
-println(data[1])
-println(sizeof(data))
-println(sizeof(data2))
-println(typeof(data))
-println("Done appending to data\n")
-
-data3 = hcat(data, data2)
-# something = reshape(data3, (2, 1234321))
-println(ndims(data3))
-println(typeof(data3))
-
-println("Converting to dataframe...")
+dataSetFolder = "/raided/datasets/sketches_png/png/"
 df = DataFrame(Any, 0, 1234322)
-push!(df, data)
-# push!(df, data2)
 
-# println(head(df))
-# df2 = convert(DataFrame, data3)
-# println(head(df2))
-println("Done converting to dataframe\n")
+listOfImgs = open(dataSetFolder * "filelist.txt")
+imgPaths = readlines(listOfImgs)
+close(listOfImgs)
 
-println("Writing out to csv...")
-writetable("asdf.csv", df)
-println("Done writing\n")
+writetable("/raided/datasets/sketches_png/allSketches.csv", df)
 
-println("Reading in csv...")
-p = readtable("asdf.csv")
+output = open("/raided/datasets/sketches_png/allSketches.csv", "a")
 
-# println(head(p'))
+count = 0
+for path in imgPaths
+    path = path[1:end-1]
+    sketchName = split(path, "/")[1]
 
+    println("Loading picture " * dataSetFolder * path * "... ")
+    img = load(dataSetFolder * path)
+    println("Done loading picture. Now extracting pixel values...\n")
+
+    println("Converting image to array of strings...")
+    img = map(x-> round(Int64, x.val), img)
+    println("img is now an array of strings\n")
+
+    println("Reshape img to 1d array...")
+    flatten = reshape(img, 1234321)
+    println("Done reshaping\n")
+
+    println("Appending tranpose of image to data and convert to (1,21234322) array...")
+    data = vcat([sketchName], flatten)
+    data = reshape(data, (1, 1234322))
+    println("Done appending to data\n")
+
+    println("Adding to dataframe...")
+    # push!(df, flatten)
+    println("Done adding to dataframe\n")
+    
+    println("Writing out to csv...")
+    println(sizeof(data))
+    println(ndims(data))
+    writecsv(output, data)
+    println("Done writing\n")
+end
+
+close(output)
+
+println("********************************")
+println("DONE")
+println("********************************")
